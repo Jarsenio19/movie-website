4c8beba6eef8b7b0b3daf81bf67194fc4845bda4
Jayson Arsenio <jay.arsenio19@gmail.com>
2026-02-25 16:55:30 +0800

    add new files

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
-navToggle.addEventListener('click', () => {
-    primaryNav.hasAttribute('data-visible' ) 
-    ? navToggle.setAttribute('aria-expanded', true)
-    : navToggle.setAttribute('aria-expanded', false)
-    primaryNav.toggleAttribute("data-visible");
-});
 
+navToggle.addEventListener('click', () => {
+    if (primaryNav.hasAttribute('data-visible')) {
+        navToggle.setAttribute('aria-expanded', false);
+    } else {
+        navToggle.setAttribute('aria-expanded', true);
+    }
+    primaryNav.toggleAttribute("data-visible");
+    });
